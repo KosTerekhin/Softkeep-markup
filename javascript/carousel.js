5c75be42749bbe55311669d9a83f57274b71f7c0
Kos Terekhin <terekhin.konst@gmail.com>
2020-06-03 11:53:19 +0300

    1440 width fixes

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -287,12 +287,7 @@ owl.owlCarousel({
 });
 // AUDIO CAROUSELS
 
-$(document).ready(function() {
-	$('#body-carousel-audio-1').owlCarousel();
-});
-
-var owl1 = $('#body-carousel-audio-1');
-owl1.owlCarousel({
+const audioObj = {
 	items: 6,
 	margin: 0,
 	loop: true,
@@ -300,7 +295,6 @@ owl1.owlCarousel({
 	nav: true,
 	mouseDrag: true,
 	touchDrag: true,
-
 	responsive: {
 		0: {
 			items: 1
@@ -311,105 +305,37 @@ owl1.owlCarousel({
 		899: {
 			items: 3
 		},
-		1441: {
+		1200: {
 			items: 4
 		},
 		1800: {
 			items: 6
 		}
 	}
+};
+
+$(document).ready(function() {
+	$('#body-carousel-audio-1').owlCarousel();
 });
+
+var owl1 = $('#body-carousel-audio-1');
+owl1.owlCarousel(audioObj);
 
 $(document).ready(function() {
 	$('#body-carousel-audio-2').owlCarousel();
 });
 
 var owl = $('#body-carousel-audio-2');
-owl.owlCarousel({
-	items: 6,
-	margin: 0,
-	loop: true,
-	dots: false,
-	nav: true,
-	mouseDrag: true,
-	touchDrag: true,
-	responsive: {
-		0: {
-			items: 1
-		},
-		767: {
-			items: 2
-		},
-		899: {
-			items: 3
-		},
-		1441: {
-			items: 4
-		},
-		1800: {
-			items: 6
-		}
-	}
-});
+owl.owlCarousel(audioObj);
 $(document).ready(function() {
 	$('#body-carousel-audio-3').owlCarousel();
 });
 
 var owl = $('#body-carousel-audio-3');
-owl.owlCarousel({
-	items: 6,
-	margin: 0,
-	loop: true,
-	dots: false,
-	nav: true,
-	mouseDrag: true,
-	touchDrag: true,
-	responsive: {
-		0: {
-			items: 1
-		},
-		767: {
-			items: 2
-		},
-		899: {
-			items: 3
-		},
-		1441: {
-			items: 4
-		},
-		1800: {
-			items: 6
-		}
-	}
-});
+owl.owlCarousel(audioObj);
 $(document).ready(function() {
 	$('#body-carousel-audio-4').owlCarousel();
 });
 
 var owl = $('#body-carousel-audio-4');
-owl.owlCarousel({
-	items: 6,
-	margin: 0,
-	loop: true,
-	dots: false,
-	nav: true,
-	mouseDrag: true,
-	touchDrag: true,
-	responsive: {
-		0: {
-			items: 1
-		},
-		767: {
-			items: 2
-		},
-		899: {
-			items: 3
-		},
-		1441: {
-			items: 4
-		},
-		1800: {
-			items: 6
-		}
-	}
-});
+owl.owlCarousel(audioObj);
